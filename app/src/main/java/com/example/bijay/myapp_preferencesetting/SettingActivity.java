c9a1fcefc22e24f0436ec433e82bdf55336a07dd
bijay kumar swain <i.bijay@gmail.com>
2016-05-27 10:29:19 +0530

    Signed-off-by: bijay kumar swain <i.bijay@gmail.com>

--- app/src/main/java/com/example/bijay/myapp_preferencesetting/SettingActivity.java
+++ app/src/main/java/com/example/bijay/myapp_preferencesetting/SettingActivity.java
@@ -1,15 +1,48 @@
 package com.example.bijay.myapp_preferencesetting;
 
-import android.preference.PreferenceActivity;
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.TargetApi;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
-public class SettingActivity extends PreferenceActivity {
+public class SettingActivity extends PreferenceActivity{
+    private static int prefs=R.xml.preferece;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-        addPreferencesFromResource(R.xml.preferece);
+        try {
+            getClass().getMethod("getFragmentManager");
+            AddResourceApi11AndGreater();
+        } catch (NoSuchMethodException e) { //Api < 11
+            AddResourceApiLessThan11();
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    protected void AddResourceApiLessThan11()
+    {
+        addPreferencesFromResource(prefs);
+    }
+
+    @TargetApi(11)
+    protected void AddResourceApi11AndGreater()
+    {
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new PF()).commit();
+    }
+
+    @TargetApi(11)
+    public static class PF extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(SettingActivity.prefs); //outer class
+            // private members seem tobe visible for inner class, and
+            // making it static made things so much easier
+        }
     }
 }
